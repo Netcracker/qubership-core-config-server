bb03b0b38559fea9251e15d2afb0fede39ffc5ba
Evgenii Stetsenko <i@estetsenko.com>
2025-08-08 14:22:24 +0300

    fix: add in-mem database configuration for tests

--- config-server-app/src/test/java/org/qubership/cloud/configserver/load/config/JpaConfiguration.java
+++ config-server-app/src/test/java/org/qubership/cloud/configserver/load/config/JpaConfiguration.java
@@ -1,10 +1,10 @@
 package org.qubership.cloud.configserver.load.config;
 
-import org.h2.jdbcx.JdbcDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
@@ -15,13 +15,10 @@ public class JpaConfiguration {
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        LocalContainerEntityManagerFactoryBean em
-                = new LocalContainerEntityManagerFactoryBean();
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
         em.setPackagesToScan("org.qubership.cloud.configserver.config");
-
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
 
         return em;
     }
@@ -29,8 +26,9 @@ public class JpaConfiguration {
     @Bean
     @Primary
     public DataSource testDataSource() {
-        JdbcDataSource ds = new JdbcDataSource();
-        ds.setURL("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1");
-        return ds;
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("testdb")
+                .build();
     }
 }
