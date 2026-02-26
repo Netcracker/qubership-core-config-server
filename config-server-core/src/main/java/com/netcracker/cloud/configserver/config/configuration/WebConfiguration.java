93846da451c9aabe78addd952c6f644ffebe8935
Evgenii Stetsenko <i@estetsenko.com>
2026-02-26 14:50:01 +0300

    feat: migrate to Java 25, Spring Framework 7, JUnit 6

--- config-server-core/src/main/java/com/netcracker/cloud/configserver/config/configuration/WebConfiguration.java
+++ config-server-core/src/main/java/com/netcracker/cloud/configserver/config/configuration/WebConfiguration.java
@@ -7,9 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.UrlHandlerFilter;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
 public class WebConfiguration {
