77762993b37ed049b68483d4ff59bab0918627df
Vladislav Larkin <larkin.qubership@gmail.com>
2026-03-23 10:30:44 +0400

    revert: "feat: migrate to Java 25, Spring Framework 7, JUnit 6 (#137)"
    This reverts commit c59a83c8

--- config-server-core/src/main/java/com/netcracker/cloud/configserver/config/configuration/WebConfiguration.java
+++ config-server-core/src/main/java/com/netcracker/cloud/configserver/config/configuration/WebConfiguration.java
@@ -4,16 +4,24 @@ import com.netcracker.cloud.restclient.MicroserviceRestClientFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.UrlHandlerFilter;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public UrlHandlerFilter urlHandlerFilter() {
-        return UrlHandlerFilter.trailingSlashHandler("/**")
-                .wrapRequest()
-                .build();
+
+    /*
+    Spring Boot 3 significantly changed the trailing slash matching configuration
+    option. This option determines whether or not to treat a URL with a trailing slash
+    the same as a URL without one. Previous versions of Spring Boot set this option
+    to true by default. This meant that a controller would match both
+    “GET /some/greeting” and “GET /some/greeting/” by default.
+    We set the old behaviour.
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseTrailingSlashMatch(true);
     }
 
     @Bean("simpleMicroserviceRestClientFactory")
